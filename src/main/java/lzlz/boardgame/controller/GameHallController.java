9ad463591dbd49bd93d54ed99fb2c29789a8ce61
lzlz <lzlz0810@qq.com>
2018-01-24 16:47:19 +0800

    下班

--- src/main/java/lzlz/boardgame/controller/GameHallController.java
+++ src/main/java/lzlz/boardgame/controller/GameHallController.java
@@ -1,8 +1,15 @@
 package lzlz.boardgame.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("game")
 public class GameHallController {
+    @GetMapping("/hall")
+    public String hall() {
 
+        return "forward:/static/html/game/hall.html";
+    }
 }
