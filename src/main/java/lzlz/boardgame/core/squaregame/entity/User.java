26656bef279a8033458ca7496c47070b9cd7b9c2
lzlz <lzlz0810@qq.com>
2018-02-05 09:20:30 +0800

    去除不需要的属性

--- src/main/java/lzlz/boardgame/core/squaregame/entity/User.java
+++ src/main/java/lzlz/boardgame/core/squaregame/entity/User.java
@@ -1,26 +1,16 @@
 package lzlz.boardgame.core.squaregame.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lzlz.boardgame.constant.PlayerState;
-import lzlz.boardgame.constant.UserRole;
 import lzlz.boardgame.core.squaregame.PlayerRole;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
     String id;
-    UserRole role;
     String name;
     PlayerRole playerRole;
     PlayerState state;
     int score;
-    @JsonIgnore
-    String chatSessionId;
-    @JsonIgnore
-    String gameSessionId;
 }
